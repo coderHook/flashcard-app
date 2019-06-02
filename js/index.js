61deda98d659000723ef7e7b01d688cce28f96b2
coderhook <abel1987@hotmail.es>
2019-06-02 22:53:55 +0200

    Fixed answer not shoewing correctly on hover

--- js/index.js
+++ js/index.js
@@ -1,71 +1,4 @@
 //Insert data. (In the future we shoudl export it form another file or API)
-
-let jsq = [
-    {
-        question: '',
-        answer: ''
-    },
-    { question:  "Inside which HTML element do we put the JavaScript?",
-      questionType:  2,
-                correctChoice: "script",
-                score: 10,
-    },
-    { question:  "Where is the correct place to insert a JavaScript?",
-      questionType:  1,
-                choices: [ "head", "body", "both"],
-                correctChoice: "both",
-                score: 5,
-    },
-    { question:  "How to write an IF statement in JavaScript?",
-      questionType:  1,
-                choices: [ "if (i == 5)", "if (i = 5)", "if i = 5"],
-                correctChoice: "if (i == 5)",
-                score: 5,
-    },
-    { question:  "Which event occurs when the user clicks on an HTML element?",
-      questionType:  1,
-      choices: [ "onchange", "onmousehover", "onclick"],
-      correctChoice: "onclick",
-      score: 5,
-    },
-    { question:  "How do you declare a JavaScript variable?",
-      questionType:  1,
-      choices: [ "var car", "v car", "variable car"],
-      correctChoice: "var car",
-      score: 5,
-    },
-    { question:  "The Tag is used To Give Heading To The Table",
-      questionType:  1,
-      choices: [ "td", "tr", "thead"],
-      correctChoice: "td",
-      score: 5,
-    },
-    { question:  "Which Of The Dialog Box Display a Message And a Data Entry Field",
-      questionType:  1,
-      choices: [ "consolelog", "promt", "alert"],
-      correctChoice: "alert",
-      score: 5,
-    },
-    { question:  "Function is Used To Parse a String To Int",
-      questionType:  1,
-      choices: [ "intparse", "parseint", "toint"],
-      correctChoice: "parseint",
-      score: 5,
-    },
-    { question:  "Java Script File Has An Extension Of",
-      questionType:  1,
-      choices: [ "js", "j", "jsc"],
-      correctChoice: "js",
-      score: 5,
-    },
-    { question:  "If Button is clicked .......Event Handler is invoked",
-      questionType:  1,
-      choices: [ "OnSubmit", "Onload", "OnClick"],
-      correctChoice: "OnClick",
-      score: 5,
-    }
-  ];
-
 let data = [
     {
         question: 'A question will appear once you clicked the button, his Answer will appear when you hover over the card',
@@ -90,18 +23,21 @@ let data = [
     }
 ]
 
-let count = 0;
+let count;
 
-nextQuestion();
+initialize();
 
 /* - - - - - F U N C T I O N S - - - - - - - - */
+function initialize(){
+    count = 0;
+}
+
 function nextQuestion(){
     console.log('im here');
+    count++
     let question = document.querySelector('.question-title');
     question.innerText = data[count].question ;
-
-    count++;
-
+    
     console.log(question.innerText);
 }
 
